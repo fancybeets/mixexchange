af6203caffc21e23454825dcb1b6832eee23099b
Aaron Taylor <hobo1990@gmail.com>
2016-04-28 03:51:54 +0000

    Fixed a typo in environment.rb so I can use sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
 ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
@@ -10,5 +11,5 @@ ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
-    :enable_starttls_auto => true
+    :enable_startstls_auto => true
 }

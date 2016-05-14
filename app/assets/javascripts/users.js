d845c5e4402e28b89bd5326b20ceb1f670f58cea
Aaron Taylor <hobo1990@gmail.com>
2016-05-14 22:58:40 +0000

    modified devise for stripe

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+    //Watch for form submission:
+    $("#form-submission-btn").click(function(event) {
+        event.preventDefault();
+        $('input[type=submit]').prop('disabled',true);
+        var error = false;
+        var ccNum = $('#card_number').val(),
+            cvcNum = $('#card_code').val(),
+            expMonth = $('#card_month').val(),
+            expYear = $('#card_year').val();
+            
+        if (!error) {
+            //Get the stripe token:
+            Stripe.createToken({
+                number: ccNum,
+                cvc: cvcNum,
+                exp_month: expMonth,
+                exp_year: expYear
+            }, stripeResponseHandler);
+        }
+        return false;
+    }); // form submission
+    
+    function stripeResponseHandler(status, response) {
+        var f = $('#new_user');
+        var token = response.id;
+        f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+        f.get(0).submit();
+    }
+});

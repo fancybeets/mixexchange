a5d855dc483fb9674c5afec5eecd74864e2f9113
Aaron Taylor <hobo1990@gmail.com>
2016-06-09 02:25:35 +0000

    reworked profile page to be more user friendly / responsive

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -16,9 +16,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     
     private
         def select_plan
-            unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
-                flash[:notice] = "Please select a membership plan to sign up."
-                redirect_to root_url
-            end
+
         end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
+        unless Profile.exists?(user_id: @user.id)
+            if current_user.id == @user.id
+                redirect_to new_user_profile_path(current_user.id)
+            else
+                redirect_to root_path
+            end
+        end
     end
 end

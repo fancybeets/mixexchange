2f715ec1bca9e0c690fddda972ba31e64e3867e3
Aaron Taylor <hobo1990@gmail.com>
2016-06-25 16:14:48 +0000

    added paperclip gem

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
     def new
         # form where each user can fill out their own profile
         @user = User.find(params[:user_id])
@@ -17,11 +19,32 @@ class ProfilesController < ApplicationController
     end
     
     def edit
-        
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+    end
+    
+    def update
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params)
+            flash[:success] = 'Profile updated!'
+            redirect_to user_path(params[:user_id])
+        else
+            flash[:notice] = 'Failed to update profile.'
+            render action :edit
+        end
     end
 
     private
     def profile_params
         params.require(:profile).permit(:first_name,:last_name,:mailing_list,:description)
+    end
+    
+    def only_current_user
+        @user = User.find(params[:user_id])
+        unless @user == current_user
+            flash[:notice] = 'You can only edit your own profile.'
+            redirect_to(root_url)
+        end
     end
 end
